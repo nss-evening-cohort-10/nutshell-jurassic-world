2ab6b25bd8f1dc9f490cf8eebf76299ee155047a
Laura Collins <laura.e.collins1@gmail.com>
2019-11-14 19:53:45 -0600

    navbar done. Login button swaps to logout button - auth not connected yet

--- src/javascripts/components/navBar/navBar.js
+++ src/javascripts/components/navBar/navBar.js
@@ -1,4 +1,5 @@
 import './navBar.scss';
+import $ from 'jquery';
 import logo from './assets/logo.png';
 import googleLogo from './assets/googleLogo.png';
 import utilities from '../../helpers/utilities';
@@ -8,9 +9,17 @@ const printLogo = () => {
   utilities.printToDom('brandLogo', domString);
 };
 
+const logInEvent = () => {
+  const domString = '<button class="btn btn-dark" id="logOutButton">Logout</button>';
+  utilities.printToDom('logButtons', domString);
+  // eslint-disable-next-line no-use-before-define
+  $('body').on('click', '#logOutButton', printLoginButton);
+};
+
 const printLoginButton = () => {
   const domString = `<button class="btn btn-dark" id="logInButton"><img src="${googleLogo}"> Login</button>`;
   utilities.printToDom('logButtons', domString);
+  $('body').on('click', '#logInButton', logInEvent);
 };
 
 export default { printLogo, printLoginButton };
