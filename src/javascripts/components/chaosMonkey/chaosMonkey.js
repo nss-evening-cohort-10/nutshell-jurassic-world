71920d7c9a784d0670befe580aafeeb3b6d5ca3c
Monica Djunaidi <modjun12@gmail.com>
2019-11-18 21:21:14 -0600

    wip

--- src/javascripts/components/chaosMonkey/chaosMonkey.js
+++ src/javascripts/components/chaosMonkey/chaosMonkey.js
@@ -2,7 +2,23 @@ import './chaosMonkey.scss';
 import $ from 'jquery';
 import monkeyImg from './assets/images/chaosMonkey.gif';
 import utilities from '../../helpers/utilities';
+import staffData from '../../helpers/data/staffData';
 
+const kidnapStaff = () => {
+  let domString = '';
+  staffData.getStaff()
+    .then((allStaff) => {
+      const newAllStaff = { ...allStaff };
+      const randomStaff = Math.floor(Math.random() * allStaff.length);
+      if (newAllStaff[randomStaff].statusId === 'status1') {
+        newAllStaff[randomStaff].statusId = 'status2';
+        const staffName = newAllStaff[randomStaff].name;
+        domString += `kidnapped ${staffName}`;
+      }
+      return domString;
+    })
+    .catch((error) => console.error(error));
+};
 
 const cron = require('cron');
 
@@ -15,17 +31,19 @@ const chaosMonkeyData = (monkeyDamage) => {
 };
 
 const randomMonkeyEvent = () => {
-  const attackZone = Math.floor((Math.random() * 3) + 1);
+  // const attackZone = Math.floor((Math.random() * 3) + 1);
+  const attackZone = 2;
   return attackZone;
 };
 
-const chaosMonkey = cron.job('40 19 * * 0-6', () => {
+const chaosMonkey = cron.job('18 21 * * 0-6', () => {
   const attackZone = randomMonkeyEvent();
+  const kindappingStaff = kidnapStaff();
   let domString = '';
   if (attackZone === 1) {
     domString = 'fill in ride event';
   } else if (attackZone === 2) {
-    domString = 'fill in equipment event';
+    domString = kindappingStaff;
   } else if (attackZone === 3) {
     domString = 'fill in staff event';
   }
