aefe1d25c2cd8e36776d54a1d52c79c653c9ece5
Kelsey Creel <kelseycreel@gmail.com>
2019-11-19 18:57:30 -0600

    WIP

--- src/javascripts/components/chaosMonkey/chaosMonkey.js
+++ src/javascripts/components/chaosMonkey/chaosMonkey.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 import monkeyImg from './assets/images/chaosMonkey.gif';
 import utilities from '../../helpers/utilities';
 import rideData from '../../helpers/data/rideData';
+import equipmentData from '../../helpers/data/equipmentData';
 
 const cron = require('cron');
 
@@ -36,6 +37,23 @@ const rideBreaker = () => rideData.getRides().then((rides) => {
   return rideName;
 }).catch((error) => console.error(error));
 
+const equipUpdater = () => console.log('yes');
+
+const equipBreaker = () => equipmentData.getEquipmentData().then((equipments) => {
+  const number = equipments.length;
+  const attackedEquip = Math.floor((Math.random() * number));
+  const equipId = equipments[attackedEquip].id;
+  const equipName = equipments[attackedEquip].type;
+  const updatedEquip = {
+    type: `${equipments[attackedEquip].type}`,
+    description: `${equipments[attackedEquip].description}`,
+    status: 'status2',
+    quantity: `${equipments[attackedEquip].quantity}`,
+  };
+  equipUpdater(equipId, updatedEquip);
+  return equipName;
+}).catch((error) => console.error(error));
+
 const chaosMonkey = cron.job('26 21 * * 0-6', () => {
   const attackZone = randomMonkeyEvent();
   let domString = '';
@@ -47,6 +65,7 @@ const chaosMonkey = cron.job('26 21 * * 0-6', () => {
   } else if (attackZone === 2) {
     domString = 'fill in equipment event';
   } else if (attackZone === 3) {
+    equipBreaker();
     domString = 'fill in staff event';
   }
 });
