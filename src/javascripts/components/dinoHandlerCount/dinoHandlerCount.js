87ee4f20ff23d7afc7e0573e0d82ef19a0b38d7a
Maggie Greene <maggie.greene96@gmail.com>
2019-12-02 18:17:02 -0600

    Work in Progress

--- src/javascripts/components/dinoHandlerCount/dinoHandlerCount.js
+++ src/javascripts/components/dinoHandlerCount/dinoHandlerCount.js
@@ -3,9 +3,9 @@ import dinoStaffData from '../../helpers/data/dinoStaffData';
 const dinoAlert = () => {
   dinoStaffData.getDinoStaff()
     .then((dinoStaff) => {
+      const newArray = [];
       dinoStaff.forEach((dinoEmployee) => {
         if (dinoEmployee.dinoId === 'dino1') {
-          const newArray = [];
           newArray.push(dinoEmployee);
           console.error(newArray, 'from handler count file');
         }
