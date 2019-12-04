e450ba323be385bf315cdd812fe5f8b285fca052
Crystal Broach <crysland@gmail.com>
2019-12-03 23:50:08 -0600

    deleted excess file that wasn't used

--- src/javascripts/helpers/data/staffSmash.js
+++
@@ -1,5 +0,0 @@
-
-// 3. Get id for ride if staff member is assigned
-// 4. Delete the rideStaff records that are found
-// 5. Get id for dino assignment if staff is assigned
-// 6. Delete the dinoStaff record that is found
